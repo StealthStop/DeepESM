0ec214936c24d83843f63bc4797b5f82b47da612
Christopher_Madrid <Chris_Madrid@baylor.edu>
2018-09-10 19:30:00 -0500

    Now using keras rather than tf.keras and is working really well now

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import os
 import tensorflow as tf
+import keras
 import numpy as np
 import pandas as pd
 from DataGetter import DataGetter as dg
@@ -8,25 +9,25 @@ from glob import glob
 
 # Makes a fully connected DNN 
 def create_main_model(n_var, n_first_layer, n_hidden_layers, n_last_layer, drop_out):
-    inputs = tf.keras.layers.Input(shape=(n_var,))
-    M_layer = tf.keras.layers.Dense(n_first_layer, activation='relu')(inputs)
+    inputs = keras.layers.Input(shape=(n_var,))
+    M_layer = keras.layers.Dense(n_first_layer, activation='relu')(inputs)
     for n in n_hidden_layers:
-        M_layer = tf.keras.layers.Dense(n, activation='relu')(M_layer)
-    M_layer = tf.keras.layers.Dropout(drop_out)(M_layer)
-    M_layer = tf.keras.layers.Dense(n_last_layer, activation='relu')(M_layer)
-    mainModel = tf.keras.models.Model(inputs=inputs, outputs=M_layer)
+        M_layer = keras.layers.Dense(n, activation='relu')(M_layer)
+    M_layer = keras.layers.Dropout(drop_out)(M_layer)
+    M_layer = keras.layers.Dense(n_last_layer, activation='relu')(M_layer)
+    mainModel = keras.models.Model(inputs=inputs, outputs=M_layer)
     return mainModel
 
 # Define loss functions
 def make_loss_model(c):
     def loss_model(y_true, y_pred):
-        return c * tf.keras.backend.binary_crossentropy(y_true, y_pred)
+        return c * keras.backend.binary_crossentropy(y_true, y_pred)
     return loss_model
 
 def make_loss_adversary(c):
     def loss_adversary(y_true, y_pred):
-        #return c * tf.keras.backend.categorical_crossentropy(y_true, y_pred)
-        return c * tf.keras.backend.binary_crossentropy(y_true, y_pred)
+        #return c * keras.backend.categorical_crossentropy(y_true, y_pred)
+        return c * keras.backend.binary_crossentropy(y_true, y_pred)
     return loss_adversary
     
 # Takes training vars, signal and background files and returns training data
@@ -74,6 +75,8 @@ if __name__ == '__main__':
     #           "Jet_eta_1","Jet_eta_2","Jet_eta_3","Jet_eta_4","Jet_eta_5","Jet_eta_6",
     #           "Jet_phi_1","Jet_phi_2","Jet_phi_3","Jet_phi_4","Jet_phi_5","Jet_phi_6",
     #           "Jet_m_1", "Jet_m_2", "Jet_m_3", "Jet_m_4", "Jet_m_5", "Jet_m_6"]
+    #allVars = ["fwm2_top6", "fwm3_top6", "fwm4_top6", "fwm5_top6", "jmt_ev0_top6", "jmt_ev1_top6", "jmt_ev2_top6"]
+
     
     # Import data
     print("----------------Preparing data------------------")
@@ -95,57 +98,60 @@ if __name__ == '__main__':
 
     # Make and train model
     #model = create_main_model(n_var=trainData["data"].shape[1], n_first_layer=70, n_hidden_layers=[70, 70, 70], n_last_layer=1, drop_out=0.5)
-    #adagrad = tf.keras.optimizers.Adagrad(lr=0.01, epsilon=None, decay=0.0)
+    #adagrad = keras.optimizers.Adagrad(lr=0.01, epsilon=None, decay=0.0)
     #model.compile(loss=make_loss_model(c=1.0), optimizer=adagrad, metrics=['accuracy'])
     #os.makedirs("TEST")
-    #log_model = tf.keras.callbacks.ModelCheckpoint('TEST/BestNN.hdf5', monitor='val_loss', verbose=1, save_best_only=True)
+    #log_model = keras.callbacks.ModelCheckpoint('TEST/BestNN.hdf5', monitor='val_loss', verbose=1, save_best_only=True)
     #result_log = model.fit(trainData["data"], trainData["labels"][:,0], batch_size=2048, epochs=200, validation_data=(testData["data"], testData["labels"][:,0]), callbacks=[log_model])
     
     print("----------------Preparing training model------------------")
-    lam = gr_lambda = 0.0
+    lam = 1
+    gr_lambda = 6
     nNodes = 70
+    nNodesD = 10
     n_hidden_layers = list(nNodes for x in range(3))
-    drop_out = 0.5
-    adagrad = tf.keras.optimizers.Adagrad(lr=0.01, epsilon=None, decay=0.0)
-    epochs=100
+    n_hidden_layers_D = list(nNodesD for x in range(5))
+    drop_out = 0.8
+    adagrad = keras.optimizers.Adagrad(lr=0.01, epsilon=None, decay=0.0)
+    epochs=200    
     
-    main_input = tf.keras.layers.Input(shape=(trainData["data"].shape[1],), name='main_input')
-    mean = tf.keras.backend.constant(value=trainData["mean"], dtype=np.float32)
-    scale = tf.keras.backend.constant(value=trainData["scale"], dtype=np.float32)
+    main_input = keras.layers.Input(shape=(trainData["data"].shape[1],), name='main_input')
+    mean = keras.backend.constant(value=trainData["mean"], dtype=np.float32)
+    scale = keras.backend.constant(value=trainData["scale"], dtype=np.float32)
     # Get the rescale inputs to have unit variance centered at 0 between -1 and 1
-    layer = tf.keras.layers.Lambda(lambda x: (x - mean) * scale, name='normalizeData')(main_input)
-    #layer = tf.keras.layers.BatchNormalization()(layer)
-    layer = tf.keras.layers.Dense(nNodes, activation='relu')(layer)
+    layer = keras.layers.Lambda(lambda x: (x - mean) * scale, name='normalizeData')(main_input)
+    #layer = keras.layers.BatchNormalization()(layer)
+    layer = keras.layers.Dense(nNodes, activation='relu')(layer)
     for n in n_hidden_layers:
-        layer = tf.keras.layers.Dense(n, activation='relu')(layer)
-    layer = tf.keras.layers.Dropout(drop_out)(layer)
-    first_output = tf.keras.layers.Dense(1, activation='relu', name='first_output')(layer)
+        layer = keras.layers.Dense(n, activation='relu')(layer)
+    layer = keras.layers.Dropout(drop_out)(layer)
+    first_output = keras.layers.Dense(1, activation='sigmoid', name='first_output')(layer)
     
-    #layer = tf.keras.layers.BatchNormalization()(first_output) #Might not need this
-    #Flip = GradientReversal(gr_lambda)
-    #layer = Flip(layer)
-    layer = tf.keras.layers.Dense(nNodes, activation='relu')(first_output)
-    for n in n_hidden_layers:
-        layer = tf.keras.layers.Dense(n, activation='relu')(layer)
-    layer = tf.keras.layers.Dropout(drop_out)(layer)
-    second_output = tf.keras.layers.Dense(1, activation='relu', name='second_output')(layer)
+    #layer = keras.layers.BatchNormalization()(first_output) #Might not need this
+    Flip = GradientReversal(gr_lambda)
+    layer = Flip(first_output)
+    layer = keras.layers.Dense(nNodesD, activation='relu')(layer)
+    for n in n_hidden_layers_D:
+        layer = keras.layers.Dense(n, activation='relu')(layer)
+    layer = keras.layers.Dropout(drop_out)(layer)
+    second_output = keras.layers.Dense(1, activation='relu', name='second_output')(layer)
     
-    model = tf.keras.models.Model(inputs=main_input, outputs=[first_output, second_output], name='model')
-    model.compile(loss=[make_loss_model(c=1.0) , make_loss_adversary(c=-lam)], optimizer=adagrad, metrics=['accuracy'], loss_weights=[1.0, 1.0])
+    model = keras.models.Model(inputs=main_input, outputs=[first_output, second_output], name='model')
+    model.compile(loss=[make_loss_model(c=1.0) , make_loss_adversary(c=lam)], optimizer=adagrad, metrics=['accuracy'], loss_weights=[1.0, 1.0])
     os.makedirs("TEST/log_graph")
-    tbCallBack = tf.keras.callbacks.TensorBoard(log_dir='./TEST/log_graph', histogram_freq=0, write_graph=True, write_images=True)
-    log_model = tf.keras.callbacks.ModelCheckpoint('TEST/BestNN.hdf5', monitor='val_loss', verbose=1, save_best_only=True)
-    result_log = model.fit(trainData["data"], [trainData["labels"][:,0], trainData["domain"][:,0]], batch_size=2048, epochs=epochs,
+    tbCallBack = keras.callbacks.TensorBoard(log_dir='./TEST/log_graph', histogram_freq=0, write_graph=True, write_images=True)
+    log_model = keras.callbacks.ModelCheckpoint('TEST/BestNN.hdf5', monitor='val_loss', verbose=1, save_best_only=True)
+    result_log = model.fit(trainData["data"], [trainData["labels"][:,0], trainData["domain"][:,0]], batch_size=2048, epochs=epochs, 
                            validation_data=(testData["data"], [testData["labels"][:,0], testData["domain"][:,0]]), callbacks=[log_model, tbCallBack])
 
     # Model Visualization
-    tf.keras.utils.plot_model(model, to_file='TEST/model.png', show_shapes=True)
+    keras.utils.plot_model(model, to_file='TEST/model.png', show_shapes=True)
     
     # Save trainig model as a protocol buffers file
     print "Input name:", model.input.op.name.split(':')[0]
     print "Output name:", model.output[0].op.name.split(':')[0]
     saver = tf.train.Saver()
-    saver.save(tf.keras.backend.get_session(), 'TEST/keras_model.ckpt')
+    saver.save(keras.backend.get_session(), 'TEST/keras_model.ckpt')
     export_path="./TEST/"
     freeze_graph_binary = "python ~/Desktop/Research/SUSY/trainingTopTagger/ENV/lib/python2.7/site-packages/tensorflow/python/tools/freeze_graph.py"
     graph_file=export_path+"keras_model.ckpt.meta"
@@ -258,6 +264,15 @@ if __name__ == '__main__':
     for a in nJetDeepESMBins:
         print "DeepESM bin ", len(nJetDeepESMBins) - index, ": ", " NEvents: ", len(a)," bin cuts: ", sorted_y_split[index][0], " ", sorted_y_split[index][-1]
         plt.hist(a, bins=numbin, range=(binxl, binxh), histtype='step', density=True, log=True, label='Bin {}'.format(len(nJetDeepESMBins) - index))
+        index += 1
+    plt.legend(loc='upper right')
+    plt.show()
+    fig.savefig('TEST/nJet_log.png', dpi=fig.dpi)
+
+    index=0
+    fig = plt.figure()
+    for a in nJetDeepESMBins:
+        plt.hist(a, bins=numbin, range=(binxl, binxh), histtype='step', density=True, log=False, label='Bin {}'.format(len(nJetDeepESMBins) - index))
         index += 1
     plt.legend(loc='upper right')
     plt.show()
