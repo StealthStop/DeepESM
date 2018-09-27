2a586812911bdaeac8c274a75d18025531d95a02
Christopher_Madrid <Chris_Madrid@Baylor.edu>
2018-09-27 15:28:53 -0500

    Working on making training more config happy

--- train.py
+++ train.py
@@ -70,15 +70,14 @@ def get_data(allVars, signalDataSet, backgroundDataSet):
     scale(dataBg)
     return trainData, dataSig, dataBg    
 
-if __name__ == '__main__':
-    
+def train():    
     # Define vars
     allVars = ["Jet_pt_1", "Jet_pt_2", "Jet_pt_3", "Jet_pt_4", "Jet_pt_5", "Jet_pt_6", "Jet_pt_7",
                "Jet_eta_1","Jet_eta_2","Jet_eta_3","Jet_eta_4","Jet_eta_5","Jet_eta_6", "Jet_eta_7",
                "Jet_phi_1","Jet_phi_2","Jet_phi_3","Jet_phi_4","Jet_phi_5","Jet_phi_6", "Jet_phi_7",
                "Jet_m_1", "Jet_m_2", "Jet_m_3", "Jet_m_4", "Jet_m_5", "Jet_m_6", "Jet_m_7",
                "GoodLeptons_pt_1", "GoodLeptons_eta_1", "GoodLeptons_phi_1", "GoodLeptons_m_1",
-               #"lvMET_cm_pt", "lvMET_cm_eta", "lvMET_cm_phi", "lvMET_cm_m",
+               "lvMET_cm_pt", "lvMET_cm_eta", "lvMET_cm_phi", "lvMET_cm_m",
                #"NGoodJets_double"
     ]
     #allVars = ["Jet_pt_1", "Jet_pt_2", "Jet_pt_3", "Jet_pt_4", "Jet_pt_5", "Jet_pt_6",
@@ -110,23 +109,26 @@ if __name__ == '__main__':
 
     # Make and train model
     print("----------------Preparing training model------------------")
-    lam = 1
     gr_lambda = 4
-    Flip = GradientReversal(gr_lambda)
     nNodes = 70
     nNodesD = 10
-    n_hidden_layers = list(nNodes for x in range(1))
-    n_hidden_layers_D = list(nNodesD for x in range(1))
+    nHLayers = 1
+    nHLayersD = 1
     drop_out = 0.7
+    batch_size = 2048
+    epochs = 100
+    lr = 0.001
+    class_weight = {0: {0: 1.0, 1: 1.0}, 1: {0: 1.0, 1: 5.0, 2: 25.0, 3: 125.0, 4: 625.0}}    
     #optimizer = keras.optimizers.Adagrad(lr=0.01, epsilon=None, decay=0.0)
-    optimizer = keras.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
-    epochs=100
-    class_weight = {0: {0: 1.0, 1: 1.0}, 1: {0: 1.0, 1: 5.0, 2: 25.0, 3: 125.0, 4: 625.0}}
-    
+    optimizer = keras.optimizers.Adam(lr=lr, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
+    n_hidden_layers = list(nNodes for x in range(nHLayers))
+    n_hidden_layers_D = list(nNodesD for x in range(nHLayersD))
+    Flip = GradientReversal(gr_lambda)    
+
     main_input = keras.layers.Input(shape=(trainData["data"].shape[1],), name='main_input')
     mean = keras.backend.constant(value=trainData["mean"], dtype=np.float32)
     scale = keras.backend.constant(value=trainData["scale"], dtype=np.float32)
-    # Get the rescale inputs to have unit variance centered at 0 between -1 and 1
+    # Set the rescale inputs to have unit variance centered at 0 between -1 and 1
     layer = keras.layers.Lambda(lambda x: (x - mean) * scale, name='normalizeData')(main_input)
     layer = keras.layers.Dense(nNodes, activation='relu')(layer)
     for n in n_hidden_layers:
@@ -144,15 +146,15 @@ if __name__ == '__main__':
     second_output = keras.layers.Dense(trainData["domain"].shape[1], activation='softmax', name='second_output')(layer)
     
     model = keras.models.Model(inputs=main_input, outputs=[first_output, second_output], name='model')
-    os.makedirs("TEST/log_graph")    
-    model.compile(loss=[make_loss_model(c=1.0) , make_loss_adversary(c=lam)], optimizer=optimizer, metrics=['accuracy'])
-    tbCallBack = keras.callbacks.TensorBoard(log_dir='./TEST/log_graph', histogram_freq=0, write_graph=True, write_images=True)
-    log_model = keras.callbacks.ModelCheckpoint('TEST/BestNN.hdf5', monitor='val_loss', verbose=1, save_best_only=True)
-    result_log = model.fit(trainData["data"], [trainData["labels"], trainData["domain"]], batch_size=2048, epochs=epochs, class_weight=class_weight,
+    os.makedirs("Output/log_graph")    
+    model.compile(loss=[make_loss_model(c=1.0) , make_loss_adversary(c=1.0)], optimizer=optimizer, metrics=['accuracy'])
+    tbCallBack = keras.callbacks.TensorBoard(log_dir='./Output/log_graph', histogram_freq=0, write_graph=True, write_images=True)
+    log_model = keras.callbacks.ModelCheckpoint('Output/BestNN.hdf5', monitor='val_loss', verbose=1, save_best_only=True)
+    result_log = model.fit(trainData["data"], [trainData["labels"], trainData["domain"]], batch_size=batch_size, epochs=epochs, class_weight=class_weight,
                            validation_data=(testData["data"], [testData["labels"], testData["domain"]]), callbacks=[log_model, tbCallBack])    
     
     # Model Visualization
-    keras.utils.plot_model(model, to_file='TEST/model.png', show_shapes=True)
+    keras.utils.plot_model(model, to_file='Output/model.png', show_shapes=True)
     
     # Save trainig model as a protocol buffers file
     inputName = model.input.op.name.split(':')[0]
@@ -160,8 +162,8 @@ if __name__ == '__main__':
     print "Input name:", inputName
     print "Output name:", outputName
     saver = tf.train.Saver()
-    saver.save(keras.backend.get_session(), 'TEST/keras_model.ckpt')
-    export_path="./TEST/"
+    saver.save(keras.backend.get_session(), 'Output/keras_model.ckpt')
+    export_path="./Output/"
     #freeze_graph_binary = "python ~/Desktop/Research/SUSY/trainingTopTagger/ENV/lib/python2.7/site-packages/tensorflow/python/tools/freeze_graph.py"
     freeze_graph_binary = "python freeze_graph.py"
     graph_file=export_path+"keras_model.ckpt.meta"
@@ -194,7 +196,7 @@ if __name__ == '__main__':
     #    plt.legend(loc='upper right')
     #    plt.ylabel('norm')
     #    plt.xlabel(var)
-    #    fig.savefig("TEST/"+var+".png", dpi=fig.dpi)
+    #    fig.savefig("Output/"+var+".png", dpi=fig.dpi)
     #    index += 1
     #
     ## Normalize
@@ -208,7 +210,7 @@ if __name__ == '__main__':
     #    plt.legend(loc='upper right')
     #    plt.ylabel('norm')
     #    plt.xlabel("norm "+var)
-    #    fig.savefig("TEST/norm_"+var+".png", dpi=fig.dpi)
+    #    fig.savefig("Output/norm_"+var+".png", dpi=fig.dpi)
     #    index += 1
     
     # Plot loss of training vs test
@@ -219,7 +221,7 @@ if __name__ == '__main__':
     plt.ylabel('loss')
     plt.xlabel('epoch')
     plt.legend(['train', 'test'], loc='upper left')
-    fig.savefig('TEST/loss_train_val.png', dpi=fig.dpi)
+    fig.savefig('Output/loss_train_val.png', dpi=fig.dpi)
     
     fig = plt.figure()
     plt.plot(result_log.history['first_output_loss'])
@@ -228,7 +230,7 @@ if __name__ == '__main__':
     plt.ylabel('loss')
     plt.xlabel('epoch')
     plt.legend(['train', 'test'], loc='upper left')
-    fig.savefig('TEST/first_output_loss_train_val.png', dpi=fig.dpi)
+    fig.savefig('Output/first_output_loss_train_val.png', dpi=fig.dpi)
     
     fig = plt.figure()
     plt.plot(result_log.history['first_output_acc'])
@@ -237,7 +239,7 @@ if __name__ == '__main__':
     plt.ylabel('acc')
     plt.xlabel('epoch')
     plt.legend(['train', 'test'], loc='upper left')
-    fig.savefig('TEST/first_output_acc_train_val.png', dpi=fig.dpi)
+    fig.savefig('Output/first_output_acc_train_val.png', dpi=fig.dpi)
     
     fig = plt.figure()
     plt.plot(result_log.history['second_output_loss'])
@@ -246,7 +248,7 @@ if __name__ == '__main__':
     plt.ylabel('loss')
     plt.xlabel('epoch')
     plt.legend(['train', 'test'], loc='upper left')
-    fig.savefig('TEST/second_output_loss_train_val.png', dpi=fig.dpi)
+    fig.savefig('Output/second_output_loss_train_val.png', dpi=fig.dpi)
     
     fig = plt.figure()
     plt.plot(result_log.history['second_output_acc'])
@@ -255,7 +257,7 @@ if __name__ == '__main__':
     plt.ylabel('acc')
     plt.xlabel('epoch')
     plt.legend(['train', 'test'], loc='upper left')
-    fig.savefig('TEST/second_output_acc_train_val.png', dpi=fig.dpi)
+    fig.savefig('Output/second_output_acc_train_val.png', dpi=fig.dpi)
     
     # Plot discriminator distribution
     bins = np.linspace(0, 1, 50)
@@ -268,7 +270,7 @@ if __name__ == '__main__':
     plt.hist(y_Train_Bg, bins, color='xkcd:blue', alpha=0.9, histtype='step', lw=2, label='Bg Train', density=True)
     plt.hist(y_Val_Bg, bins, color='xkcd:magenta', alpha=0.9, histtype='step', lw=2, label='Bg Val', density=True)
     ax.legend(loc='best', frameon=False)
-    fig.savefig('TEST/discriminator.png', dpi=fig.dpi)
+    fig.savefig('Output/discriminator.png', dpi=fig.dpi)
 
     samples = {"Bg": [trainBg, y_Train_Bg], "Sg": [trainSg, y_Train_Sg]}
     for sample in samples:
@@ -279,12 +281,12 @@ if __name__ == '__main__':
         ax.set_title('')
         ax.set_ylabel('Norm Events')
         ax.set_xlabel('Discriminator')
-        for key in trainSample:
+        for key in sorted(trainSample.keys):
             if key.find("mask") != -1:
                 yt = y_train_Sp[trainSample[key]]
                 plt.hist(yt, bins, alpha=0.9, histtype='step', lw=2, label=sample+" Train "+key, density=True)
         plt.legend(loc='best')
-        fig.savefig("TEST/discriminator_nJet_"+sample+".png", dpi=fig.dpi)
+        fig.savefig("Output/discriminator_nJet_"+sample+".png", dpi=fig.dpi)
     
     # Plot validation roc curve
     fpr_Val, tpr_Val, thresholds_Val = roc_curve(valData["labels"][:,0], y_Val)
@@ -300,7 +302,7 @@ if __name__ == '__main__':
     plt.ylabel('True positive rate')
     plt.title('ROC curve')
     plt.legend(loc='best')
-    fig.savefig('TEST/roc_plot.png', dpi=fig.dpi)
+    fig.savefig('Output/roc_plot.png', dpi=fig.dpi)
     
     fig = plt.figure()
     plt.plot([0, 1], [0, 1], 'k--')
@@ -315,7 +317,7 @@ if __name__ == '__main__':
             auc_Train = auc(fpr_Train, tpr_Train)    
             plt.plot(fpr_Train, tpr_Train, label="Train "+key+" (area = {:.3f})".format(auc_Train))
     plt.legend(loc='best')
-    fig.savefig('TEST/roc_plot_nJet.png', dpi=fig.dpi)
+    fig.savefig('Output/roc_plot_nJet.png', dpi=fig.dpi)
     
     # Plot NJet dependance
     def plot2DVar(name, binxl, binxh, numbin, xIn, yIn, nbiny):
@@ -343,7 +345,7 @@ if __name__ == '__main__':
             
         xerr = 0.5*(xedges[1]-xedges[0])
         plt.errorbar(bin_centersx, y, xerr=xerr, yerr=ye, fmt='o', color='xkcd:red')
-        fig.savefig("TEST/"+name+"_discriminator.png", dpi=fig.dpi)        
+        fig.savefig("Output/"+name+"_discriminator.png", dpi=fig.dpi)        
         
     binxl = 7
     binxh = 12
@@ -372,7 +374,7 @@ if __name__ == '__main__':
         index += 1
     plt.hist(sortednJet, bins=numbin, range=(binxl, binxh), histtype='step', density=True, log=True, label='Total')
     plt.legend(loc='upper right')
-    fig.savefig('TEST/nJet_log.png', dpi=fig.dpi)
+    fig.savefig('Output/nJet_log.png', dpi=fig.dpi)
     
     index=0
     fig = plt.figure()
@@ -381,10 +383,13 @@ if __name__ == '__main__':
         index += 1
     plt.hist(sortednJet, bins=numbin, range=(binxl, binxh), histtype='step', density=True, log=False, label='Total')
     plt.legend(loc='upper right')
-    fig.savefig('TEST/nJet.png', dpi=fig.dpi)
+    fig.savefig('Output/nJet.png', dpi=fig.dpi)
     
     # Save useful stuff
-    np.save('TEST/deepESMbin_dis_nJet.npy', {"nJetBins" : nJetDeepESMBins, "y" : sorted_y_split, "nJet" : sortednJet})
+    np.save('Output/deepESMbin_dis_nJet.npy', {"nJetBins" : nJetDeepESMBins, "y" : sorted_y_split, "nJet" : sortednJet})
     config = {"bins" : bins, "input_output" : [inputName, outputName], "variables" : allVars}
-    with open("TEST/config.json",'w') as configFile:
+    with open("Output/config.json",'w') as configFile:
         json.dump(config, configFile, indent=4, sort_keys=True)
+
+if __name__ == '__main__':
+    train()
