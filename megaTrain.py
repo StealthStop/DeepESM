8304ca9130e73d40c1c034b0973e36597f4f64a4
Christopher_Madrid <Chris_Madrid@Baylor.edu>
2018-12-18 10:29:43 -0600

    Changed how the validation class works

--- megaTrain.py
+++ megaTrain.py
@@ -1,5 +1,6 @@
 from train import Train
 from multiprocessing import Pool
+import os
 import json
 
 def red(string):
@@ -12,31 +13,33 @@ def parallel_train(config):
      for key in sorted(config.keys()):
           name += key+"_"+str(config[key])+"_"
           
-     t = Train()
-     _, metric = t.train(config)
-     del t
+     command = "python train.py '"+str(config)+"'"
+     print command
+     os.system(command)
      
-     total = 0.0
-     for key in metric:
-          total += metric[key]
-
-     return (name, total, {"total":total, "metric": metric})
+     #total = 0.0
+     #for key in metric:
+     #     total += metric[key]
+     #
+     #return (name, total, {"total":total, "metric": metric})
+     return (name, 0.0, {"total":0.0, "metric": 0.0})
 
 if __name__ == '__main__':     
      configList = []
      index = 0
      totals = {}
      result = {}
-     for epochs in range(15, 35+5, 5):
+     for epochs in range(110, 130+5, 5):
           for nNodes in range(60, 80+5, 5):
-               for nHLayers in range(1, 3+1, 1):
-                    for drop_out in range(5, 9+1, 1):
-                         #for nNodesD in range(2, 20, 2):
-                         #for nHLayersD in range(1, 5):
-                         index += 1.0
-                         config = {"minNJetBin":7, "maxNJetBin":11, "gr_lambda":0, "nNodes":nNodes, "nNodesD":10,  "nHLayers":nHLayers, 
-                                   "nHLayersD":1, "drop_out":float(drop_out)/10.0, "batch_size":2048, "epochs":epochs, "lr":0.001, "verbose":0, "Mask":False, "Mask_nJet":7}
-                         configList.append(config)
+               for nNodesD in range(5, 15+5, 5):
+                    for nHLayers in range(1, 2+1, 1):
+                         for nHLayersD in range(1, 2+1, 1):                    
+                              for drop_out in range(6, 8+1, 1):
+                                   for Lambda in range(0, 3+1, 1):
+                                        index += 1.0
+                                        config = {"minNJetBin":7, "maxNJetBin":11, "gr_lambda":Lambda, "nNodes":nNodes, "nNodesD":nNodesD,  "nHLayers":nHLayers, 
+                                                  "nHLayersD":nHLayersD, "drop_out":float(drop_out)/10.0, "batch_size":2048, "epochs":epochs, "lr":0.001, "verbose":0, "Mask":False, "Mask_nJet":7}
+                                        configList.append(config)
      print red("Total number of trainings: " + str(index))
      print red("Estimated time: " + str(index/60.0) + " hours or " + str(index/60.0/24.0) + " days")
                        
