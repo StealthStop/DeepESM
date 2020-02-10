ca31ca4cb5a9fa1a4ed66d25f56cd747ef201d16
Alexander_Mercaldi <alexander.mercaldi@email.ucr.edu>
2020-02-10 14:35:37 -0600

    Updates to training file

--- StopRecoNN/train_gen_nn.py
+++ StopRecoNN/train_gen_nn.py
@@ -1,5 +1,5 @@
 import tensorflow
-from keras import Sequential, Model
+from keras import Sequential, Model, models
 from keras.layers import Dense, Input
 from keras.callbacks import ModelCheckpoint, EarlyStopping
 from sklearn.model_selection import train_test_split
@@ -8,6 +8,21 @@ import matplotlib
 import uproot
 import math
 from matplotlib import pyplot as plt
+from scipy import stats
+import ROOT
+
+
+
+RPV_signal_2016 = ['2016_RPV_2t6j_mStop-300','2016_RPV_2t6j_mStop-350','2016_RPV_2t6j_mStop-400','2016_RPV_2t6j_mStop-450','2016_RPV_2t6j_mStop-500','2016_RPV_2t6j_mStop-550','2016_RPV_2t6j_mStop-600','2016_RPV_2t6j_mStop-650','2016_RPV_2t6j_mStop-700','2016_RPV_2t6j_mStop-750','2016_RPV_2t6j_mStop-800','2016_RPV_2t6j_mStop-900']
+
+stealth_signal_2016=['2016_StealthSYY_2t6j_mStop-300','2016_StealthSYY_2t6j_mStop-350','2016_StealthSYY_2t6j_mStop-400','2016_StealthSYY_2t6j_mStop-450','2016_StealthSYY_2t6j_mStop-500','2016_StealthSYY_2t6j_mStop-550','2016_StealthSYY_2t6j_mStop-600','2016_StealthSYY_2t6j_mStop-650','2016_StealthSYY_2t6j_mStop-700','2016_StealthSYY_2t6j_mStop-750','2016_StealthSYY_2t6j_mStop-800','2016_StealthSYY_2t6j_mStop-850','2016_StealthSYY_2t6j_mStop-900']
+
+RPV_signal_2017 = ['2017_RPV_2t6j_mStop-300','2017_RPV_2t6j_mStop-350','2017_RPV_2t6j_mStop-400','2017_RPV_2t6j_mStop-450','2017_RPV_2t6j_mStop-500','2017_RPV_2t6j_mStop-550','2017_RPV_2t6j_mStop-600','2017_RPV_2t6j_mStop-650','2017_RPV_2t6j_mStop-700','2017_RPV_2t6j_mStop-750','2017_RPV_2t6j_mStop-800','2017_RPV_2t6j_mStop-900']
+
+stealth_signal_2017 =['2017_StealthSYY_2t6j_mStop-300','2017_StealthSYY_2t6j_mStop-350','2017_StealthSYY_2t6j_mStop-400','2017_StealthSYY_2t6j_mStop-450','2017_StealthSYY_2t6j_mStop-500','2017_StealthSYY_2t6j_mStop-550','2017_StealthSYY_2t6j_mStop-600','2017_StealthSYY_2t6j_mStop-650','2017_StealthSYY_2t6j_mStop-700','2017_StealthSYY_2t6j_mStop-750','2017_StealthSYY_2t6j_mStop-800','2017_StealthSYY_2t6j_mStop-850','2017_StealthSYY_2t6j_mStop-900']
+
+signal_path_2016 = '~/nobackup/SUSY3/CMSSW_10_2_9/src/Analyzer/Analyzer/test/condor/MC_Training_Input_2016_v3/'
+signal_path_2017 = '~/nobackup/SUSY3/CMSSW_10_2_9/src/Analyzer/Analyzer/test/condor/MC_Training_Input_2017_v3/'
 
 #class for making the input/output variable numpy arrays using uproot. jets are zero padded based on the maximum number of jets in all events
 
@@ -52,90 +67,160 @@ class input_output_var:
                 return np_array
 
 
+class Train:
+
+    def __init__(self):
 
 
+        print("<-------Setting up input variables------->")
+        for sig in range(0,len(stealth_signal_2016)):
 
-RPV_signal_2016 = ['2016_RPV_2t6j_mStop-300','2016_RPV_2t6j_mStop-350','2016_RPV_2t6j_mStop-400','2016_RPV_2t6j_mStop-450','2016_RPV_2t6j_mStop-500','2016_RPV_2t6j_mStop-550','2016_RPV_2t6j_mStop-600','2016_RPV_2t6j_mStop-650','2016_RPV_2t6j_mStop-700','2016_RPV_2t6j_mStop-750','2016_RPV_2t6j_mStop-800','2016_RPV_2t6j_mStop-900']
-
-stealth_signal_2016=['2016_StealthSYY_2t6j_mStop-300','2016_StealthSYY_2t6j_mStop-350','2016_StealthSYY_2t6j_mStop-400','2016_StealthSYY_2t6j_mStop-450','2016_StealthSYY_2t6j_mStop-500','2016_StealthSYY_2t6j_mStop-550','2016_StealthSYY_2t6j_mStop-600','2016_StealthSYY_2t6j_mStop-650','2016_StealthSYY_2t6j_mStop-700','2016_StealthSYY_2t6j_mStop-750','2016_StealthSYY_2t6j_mStop-800','2016_StealthSYY_2t6j_mStop-850','2016_StealthSYY_2t6j_mStop-900']
-
-RPV_signal_2017 = ['2017_RPV_2t6j_mStop-300','2017_RPV_2t6j_mStop-350','2017_RPV_2t6j_mStop-400','2017_RPV_2t6j_mStop-450','2017_RPV_2t6j_mStop-500','2017_RPV_2t6j_mStop-550','2017_RPV_2t6j_mStop-600','2017_RPV_2t6j_mStop-650','2017_RPV_2t6j_mStop-700','2017_RPV_2t6j_mStop-750','2017_RPV_2t6j_mStop-800','2017_RPV_2t6j_mStop-900']
-
-signal_path_2016 = '~/nobackup/SUSY3/CMSSW_10_2_9/src/Analyzer/Analyzer/test/condor/MC_Training_Input_2016_v3/'
-signal_path_2017 = '~/nobackup/SUSY3/CMSSW_10_2_9/src/Analyzer/Analyzer/test/condor/MC_Training_Input_2017_v3/'
-
-print("<-------Setting up input variables------->")
-for sig in range(0,len(RPV_signal_2016)):
-
-    IO_vars_2016 = input_output_var(signal_path_2016, RPV_signal_2016[sig])
-    IO_vars_2017 = input_output_var(signal_path_2017, RPV_signal_2017[sig])
-    sig_input = np.concatenate([IO_vars_2016.input,IO_vars_2017.input])
-    sig_output = np.concatenate([IO_vars_2016.output,IO_vars_2017.output])
-    if sig == 0:
-        input_variables = sig_input
-        output_variables = sig_output
-    else:
-        input_variables = np.concatenate([input_variables,sig_input])
-        output_variables = np.concatenate([output_variables,sig_output])
-
-
-background = input_output_var(signal_path_2016, '2016_TT')
-bg_input = background.input
-bg_output = background.output
+            IO_vars_2016 = input_output_var(signal_path_2016, stealth_signal_2016[sig])
+            IO_vars_2017 = input_output_var(signal_path_2017, stealth_signal_2017[sig])
+            sig_input = np.concatenate([IO_vars_2016.input,IO_vars_2017.input])
+            sig_output = np.concatenate([IO_vars_2016.output,IO_vars_2017.output])
+            if sig == 0:
+                input_variables = sig_input
+                output_variables = sig_output
+            else:
+                input_variables = np.concatenate([input_variables,sig_input])
+                output_variables = np.concatenate([output_variables,sig_output])
+            
+            
+        background = input_output_var(signal_path_2016, '2016_TT')
+        self.bg_input = background.input
+        self.bg_output = background.output
 
 
 #there are some NaNs in the output, this removes it but need to find source of issue
-check = np.argwhere(np.isnan(output_variables))
-add = 0
-for x in check[:,0]:
-    output_variables = np.delete(output_variables, x-add, 0)
-    input_variables = np.delete(input_variables, x-add, 0)
-    add += 1 
+        check = np.argwhere(np.isnan(output_variables))
+        add = 0
+        for x in check[:,0]:
+            output_variables = np.delete(output_variables, x-add, 0)
+            input_variables = np.delete(input_variables, x-add, 0)
+            add += 1 
 
 
-assert not np.any(np.isnan(input_variables))
-assert not np.any(np.isnan(output_variables))
+        assert not np.any(np.isnan(input_variables))
+        assert not np.any(np.isnan(output_variables))
 
 
-in_train_val, in_test, out_train_val, out_test = train_test_split(input_variables, output_variables, test_size=0.3, random_state=7)
+        self.in_train_val, self.in_test, self.out_train_val, self.out_test = train_test_split(input_variables, output_variables, test_size=0.3, random_state=7)
 
 
-print("Done setting up input variables. Will now begin training")
+        print("Done setting up input variables. Will now begin training")
 
-inputs = Input(shape=(np.size(in_train_val,1),), name = 'input')
-outputs = Dense(2, name = 'output', kernel_initializer='normal', activation = 'relu')(inputs)
+        inputs = Input(shape=(np.size(self.in_train_val,1),), name = 'input')
+        outputs = Dense(2, name = 'output', kernel_initializer='normal', activation = 'relu')(inputs)
 
-model = Model(inputs=inputs, outputs=outputs)
-model.compile(loss='mse',optimizer='rmsprop')
+        self.model = Model(inputs=inputs, outputs=outputs)
+        self.model.compile(loss='mse',optimizer='rmsprop')
 
-callbacks = [ EarlyStopping(verbose = True, patience = 20, monitor = 'val_loss'),
+        callbacks = [ EarlyStopping(verbose = True, patience = 20, monitor = 'val_loss'),
               ModelCheckpoint('test_model.h5',monitor='val_loss',verbose=True,save_best_only=True)]
 
-history = model.fit(in_train_val,out_train_val,epochs=200,validation_split=0.2,callbacks=callbacks)
-#print(history.history)
-#plt.plot(history.history['val_loss'], label='val_loss')
-#plt.plot(history.history['loss'], label = 'loss')
-#plt.legend()
-#plt.show()
-pred_mass = np.transpose( model.predict(in_test))
-actual_mass = np.transpose(out_test)
+        self.history = self.model.fit(self.in_train_val,self.out_train_val,epochs=200,validation_split=0.2,callbacks=callbacks)
+        
 
 
-background_pred = np.transpose( model.predict(bg_input))
+        np.save('input_test',self.in_test)
+        np.save('output_test',self.out_test)
+        self.model.save('test_model.h5')
+
+if __name__ == '__main__':
+    tr = Train()
+    model = tr.model
+    in_test = tr.in_test
+    out_test = tr.out_test
+    history = tr.history
+#    model = models.load_model('test_model.h5')
+#    in_test = np.load('input_test.npy')
+#    out_test = np.load('output_test.npy')
+
+    pred_mass = np.transpose( model.predict(in_test))
+    actual_mass = np.transpose(out_test)
+
+    background = input_output_var(signal_path_2016, '2016_TT')
+    bg_input = background.input
+    bg_output = background.output
+
+    background_pred = np.transpose( model.predict(bg_input))
+    scaled_bg_pred = np.random.choice( background_pred[0], len(pred_mass[0]))
+
+    diff_mass =  np.absolute(np.subtract(pred_mass, actual_mass))
 
 
-diff_mass =  np.absolute(np.subtract(pred_mass, actual_mass))
-fig = plt.figure()
-#plt.hist(pred_mass[0],histtype='step')
 
 
-plt.hist((pred_mass[0],actual_mass[0],background_pred[0]),label=('Predicted','Actual','Background'),histtype='step',range=(0,1500),bins=300)
-#plt.hist(diff_mass[0],histtype='step')
-plt.xlabel('Stop Mass [GeV]')
-plt.ylabel('Fraction of Events')
-plt.legend()
-plt.savefig('sig_bg_comp_RPV.png')
+    fig = plt.figure()
+    plt.plot(history.history['val_loss'], label='val_loss')
+    plt.plot(history.history['loss'], label='loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.savefig('stealth_val_plot.png')
+    plt.show()
+    
+    fig = plt.figure()
+    plt.hist((diff_mass[0],diff_mass[1]), label = ('Stop 1', 'Stop 2'), histtype = 'step', bins = 50, range=(0,500))
+    plt.xlabel('Mass Difference [GeV]')
+    plt.ylabel('Events')
+    plt.legend()
+    plt.savefig('stealth_mass_diff.png')
+    plt.show()
+
+    pred_act_weight = 1 / len(pred_mass[0])
+    bg_weight = 1 / len(scaled_bg_pred)
+    c = ROOT.TCanvas( "c", "c", 0, 0, 1200, 1200)
+
+    ROOT.TH1.SetDefaultSumw2()
+    h_pred_S1 = ROOT.TH1D("Stop mass prediction", "Stop mass prediction", 500, 0, 1500)
+    h_pred_S1.GetXaxis().SetTitle("Stop mass [GeV]")
+    h_pred_S1.GetYaxis().SetTitle("Events")
+    h_pred_S1.GetYaxis().SetRangeUser(0,1000)
+    h_pred_S1.SetStats(0)
+    h_act_S1 = ROOT.TH1D("Actual mass prediction", "Actual  mass prediction", 500, 0, 1500)
+    h_bg_S1 = ROOT.TH1D("Background Stop mass prediction", "Background Stop mass prediction", 500, 0, 1500)
+    for event in range(0,len(pred_mass[0])):
+        h_pred_S1.Fill(pred_mass[0][event])
+        h_act_S1.Fill(actual_mass[0][event])    
+        h_bg_S1.Fill(scaled_bg_pred[event])
+    h_act_S1.SetLineColor(ROOT.kRed)
+    h_bg_S1.SetLineColor(ROOT.kGreen+1)    
+    legend = ROOT.TLegend(0.6, 0.7, 0.9, 0.9)
+    legend.AddEntry(h_pred_S1, "Predicted", "l")
+    legend.AddEntry(h_act_S1, "Gen Matched", "l")
+    legend.AddEntry(h_bg_S1, "TTBar", "l")
+    h_pred_S1.Draw("h e")
+    h_act_S1.Draw("same h e")
+    h_bg_S1.Draw("same h e")
+    legend.Draw("same")
+    c.SaveAs("stealth_pred_mass_S1.pdf")
+    del c
+
+    c = ROOT.TCanvas( "c", "c", 0, 0, 1200, 1200)
+    h_pred_S2 = ROOT.TH1D("Stop mass prediction", "Stop mass prediction", 500, 0, 1500)
+    h_pred_S2.GetXaxis().SetTitle("Stop mass [GeV]")
+    h_pred_S2.GetYaxis().SetTitle("Events")
+    h_pred_S2.GetYaxis().SetRangeUser(0,1000)
+    h_pred_S2.SetStats(0)
+    h_act_S2 = ROOT.TH1D("Actual mass prediction", "Actual  mass prediction", 500, 0, 1500)
+    h_bg_S2 = ROOT.TH1D("Background Stop mass prediction", "Background Stop mass prediction", 500, 0, 1500)
+    for event in range(0,len(pred_mass[1])):
+        h_pred_S2.Fill(pred_mass[1][event])
+        h_act_S2.Fill(actual_mass[1][event])    
+        h_bg_S2.Fill(scaled_bg_pred[event])
+    h_act_S2.SetLineColor(ROOT.kRed)
+    h_bg_S2.SetLineColor(ROOT.kGreen+1)    
+    legend = ROOT.TLegend(0.6, 0.7, 0.9, 0.9)
+    legend.AddEntry(h_pred_S2, "Predicted", "l")
+    legend.AddEntry(h_act_S2, "Gen Matched", "l")
+    legend.AddEntry(h_bg_S2, "TTBar", "l")
+    h_pred_S2.Draw("h e")
+    h_act_S2.Draw("same h e")
+    h_bg_S2.Draw("same h e")
+    legend.Draw("same")
+    c.SaveAs("stealth_pred_mass_S2.pdf")
+    del c
 
 
-
-model.save('test_model.h5')
