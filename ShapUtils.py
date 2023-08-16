8ed5f037177c4ef8d2d2ac4d715e91a785e095af
rileyp336 <131489839+rileyp336@users.noreply.github.com>
2023-08-15 23:48:44 -0500

    Update ShapUtils.py

--- ShapUtils.py
+++ ShapUtils.py
@@ -5,15 +5,6 @@ import shap
 import sklearn
 import tensorflow as tf
 from shap.plots import waterfall
-
-def waterfall1(model, data_scaled, data_reg, instance_index):
-  """
-  Creates a SHAP waterfall plot for a given prediction.
-  """
-  explainer = shap.KernelExplainer(model.predict, data_scaled)
-  shap_values = explainer.shap_values(data_scaled)
-  explanation = shap.Explanation(values=shap_values[0][instance_index], base_values=explainer.expected_value[0], data=data_reg.iloc[instance_index], feature_names=data_reg.columns.tolist())
-  shap.plots.waterfall(explanation)
   
 def waterfall2(model, data, instance_index):
   """
@@ -23,3 +14,11 @@ def waterfall2(model, data, instance_index):
   shap_values = explainer.shap_values(data)
   explanation = shap.Explanation(values=shap_values[0][instance_index], base_values=explainer.expected_value[0], data=data.iloc[instance_index], feature_names=data.columns.tolist())
   shap.plots.waterfall(explanation)
+  
+def summary_plot1(model, data):
+  """
+  Creates a SHAP summary plot.
+  """
+  explainer = shap.KernelExplainer(model.predict, data)
+  shap_values = explainer.shap_values(data)
+  shap.summary_plot(shap_values, data)
