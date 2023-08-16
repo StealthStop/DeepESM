c336e2c12d60819ae0e7d824ba7ee37a2776393b
rileyp336 <131489839+rileyp336@users.noreply.github.com>
2023-08-15 23:04:28 -0500

    Update ShapUtils.py

--- ShapUtils.py
+++ ShapUtils.py
@@ -1,3 +1,5 @@
+#SHAP tools file
+
 import numpy as np
 import pandas as pd
 import shap
@@ -9,20 +11,19 @@ shap.initjs()
 from shap.plots import waterfall
 
 def waterfall(model, data_scaled, data_reg, instance_index):
-  """Creates a SHAP waterfall plot for a given prediction."""
+  """
+  Creates a SHAP waterfall plot for a given prediction.
+  """
   explainer = shap.KernelExplainer(model.predict, data_scaled)
   shap_values = explainer.shap_values(data_scaled)
-  explanation = shap.Explanation(values=shap_values[0][instance_index],
-                               base_values=explainer.expected_value[0],
-                               data=data_reg.iloc[instance_index],
-                               feature_names=data_reg.columns.tolist())
+  explanation = shap.Explanation(values=shap_values[0][instance_index], base_values=explainer.expected_value[0], data=data_reg.iloc[instance_index], feature_names=data_reg.columns.tolist())
   shap.plots.waterfall(explanation)
   
 def waterfall2(model, data, instance_index):
+  """
+  Creates a SHAP waterfall plot for a given prediction.
+  """
   explainer = shap.KernelExplainer(model.predict, data)
   shap_values = explainer.shap_values(data)
-  explanation = shap.Explanation(values=shap_values[0][instance_index],
-                               base_values=explainer.expected_value[0],
-                               data=data.iloc[instance_index],
-                               feature_names=data.columns.tolist())
+  explanation = shap.Explanation(values=shap_values[0][instance_index], base_values=explainer.expected_value[0], data=data.iloc[instance_index], feature_names=data.columns.tolist())
   shap.plots.waterfall(explanation)
