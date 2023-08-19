84018e9f29c75eae8eba90de014e64a30d5a9605
rileyp336 <131489839+rileyp336@users.noreply.github.com>
2023-08-19 15:50:07 -0500

    Update ShapUtils.py

--- ShapUtils.py
+++ ShapUtils.py
@@ -4,6 +4,7 @@ import numpy as np
 import shap
 import sklearn
 import tensorflow as tf
+import matplotlib.pyplot as plt
 from shap.plots import waterfall
   
 def waterfall2(model, data, instance_index):
@@ -14,6 +15,8 @@ def waterfall2(model, data, instance_index):
   shap_values = explainer.shap_values(data)
   explanation = shap.Explanation(values=shap_values[0][instance_index], base_values=explainer.expected_value[0], data=data.iloc[instance_index], feature_names=data.columns.tolist())
   shap.plots.waterfall(explanation)
+
+  save_plot(waterfall)
   
 def summary_plot1(model, data):
   """
@@ -22,3 +25,13 @@ def summary_plot1(model, data):
   explainer = shap.KernelExplainer(model.predict, data)
   shap_values = explainer.shap_values(data)
   shap.summary_plot(shap_values, data)
+
+def save_plot(name):
+  """
+  saves plot as 'name'.png
+  """
+  fig = plt.figure()
+  plt.savefig(name, bbox_inches ='tight', format='png')
+  plt.close()
+
+
