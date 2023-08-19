ee2fb6f6bc339dbf374102b9af30f79a4287fc0a
rileyp336 <131489839+rileyp336@users.noreply.github.com>
2023-08-19 15:34:24 -0500

    Update train.py

--- train.py
+++ train.py
@@ -24,6 +24,7 @@ from tensorflow.python.framework.convert_to_constants import convert_variables_t
 from Validation import Validation
 from Correlation import Correlation as cor
 from DataLoader import DataLoader
+from DataLoader import getFlatData
 from Models import main_model
 from MeanShiftTF import MeanShift
 from CustomCallback import CustomCallback
