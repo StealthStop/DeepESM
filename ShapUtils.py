c21615984fd3ac581091321331331b477f17c2e9
rileyp336 <rileypasse@yahoo.com>
2023-09-14 13:20:24 -0500

    Fixing data loader bug in ShapUtils

--- ShapUtils.py
+++ ShapUtils.py
@@ -11,8 +11,10 @@ def waterfall2(model, data, instance_index):
   """
   Creates a SHAP waterfall plot for a given prediction.
   """
-  explainer = shap.KernelExplainer(model.predict, data)
-  shap_values = explainer.shap_values(data)
+  inputs = data["inputs"]
+  names = data["var"]
+  explainer = shap.KernelExplainer(model.predict, inputs, feature_names=names)
+  shap_values = explainer.shap_values(inputs)
   explanation = shap.Explanation(values=shap_values[0][instance_index], base_values=explainer.expected_value[0], data=data.iloc[instance_index], feature_names=data.columns.tolist())
   shap.plots.waterfall(explanation)
 
@@ -22,9 +24,11 @@ def summary_plot1(model, data):
   """
   Creates a SHAP summary plot.
   """
-  explainer = shap.KernelExplainer(model.predict, data)
-  shap_values = explainer.shap_values(data)
-  shap.summary_plot(shap_values, data)
+  inputs = data["inputs"]
+  names = data["var"]
+  explainer = shap.KernelExplainer(model.predict, inputs, feature_names=names)
+  shap_values = explainer.shap_values(inputs)
+  shap.summary_plot(shap_values, inputs, feature_names=names)
 
 def save_plot(name):
   """
