4a4e94c33fc70a70eada2ceac839b53bf1783722
rileyp336 <131489839+rileyp336@users.noreply.github.com>
2023-09-20 09:36:46 -0500

    Update train.py
    Turned self.loader.getFlatData() into np array

--- train.py
+++ train.py
@@ -727,7 +727,7 @@ class Train:
         #work in progress
         #SHAP result for first model prediction
         
-        waterfall(model, self.loader.getFlatData(), 0)
+        waterfall(model, np.asarray(self.loader.getFlatData()), 0)
         
         metric = val.makePlots(self.doQuickVal, self.config["evalMass"], self.config["evalModel"])
         del val
