8d1ffcd8fab49e8cbe466c181631d2a9cbbfd202
rileyp336 <131489839+rileyp336@users.noreply.github.com>
2023-09-21 11:55:11 -0500

    Update ShapUtils.py

--- ShapUtils.py
+++ ShapUtils.py
@@ -21,6 +21,34 @@ def waterfall2(model, data, instance_index):
   shap.plots.waterfall(explanation)
 
   save_plot("waterfall_plot.png")
+
+def predict_with_model(model, inputs):
+    # Ensure that 'inputs' is in the correct format and shape
+    predictions = model.predict(inputs)
+    predictions = np.array(predictions)
+
+    return predictions
+
+def waterfall3(model, data, instance_index):
+    """
+    Creates a SHAP waterfall plot for a given prediction.
+    """
+    inputs = np.array(data["inputs"])
+    names = data["vars"]
+
+    # Use the modified predict_with_model function
+    explainer = shap.KernelExplainer(lambda x: predict_with_model(model, x), inputs, feature_names=names)
+    shap_values = explainer.shap_values(inputs)
+    explanation = shap.Explanation(
+        values=shap_values[0][instance_index],
+        base_values=explainer.expected_value[0],
+        data=data.iloc[instance_index],
+        feature_names=data.columns.tolist()
+    )
+    
+    shap.plots.waterfall(explanation)
+
+    save_plot("waterfall_plot.png")
   
 def summary_plot1(model, data):
   """
