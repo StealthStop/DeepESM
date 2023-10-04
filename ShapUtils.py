1a62edf21e84f9166e17613b97cc44fa01a37357
rileyp336 <131489839+rileyp336@users.noreply.github.com>
2023-10-04 13:29:12 -0500

    Added function that creates multiple plots

--- ShapUtils.py
+++ ShapUtils.py
@@ -13,23 +13,6 @@ def predict(data, model):
 
   return model.predict([data[:,i] for i in range(data.shape[1])]).flatten()
 
-def get_explination(model, data, instance_index):
-  """
-  Gets shap explanation of model prediction for a specific prediction index
-  """
-  inputs = data
-  names = data["vars"]
-
-  # Use the modified predict_with_model function
-  explainer = shap.KernelExplainer(predict(inputs, model), inputs, feature_names=names)
-  shap_values = explainer.shap_values(inputs)
-  explanation = shap.Explanation(
-      values=shap_values[0][instance_index],
-      base_values=explainer.expected_value[0],
-      data=data.iloc[instance_index],
-      feature_names=data.columns.tolist()
-  )
-  return explanation
   
 def waterfall2(model, data, instance_index):
   """
@@ -49,7 +32,7 @@ def waterfall2(model, data, instance_index):
 # See https://shap-lrjball.readthedocs.io/en/latest/example_notebooks/kernel_explainer/Census%20income%20classification%20with%20Keras.html for detailed example
 def summary_plt(model, data, numEvents):
     """
-    Creates a SHAP summary plot plot for a given prediction.
+    Creates a SHAP summary plot for a given prediction.
     """
 
     def predict_disc1(data):
@@ -86,6 +69,52 @@ def summary_plt(model, data, numEvents):
     shap.summary_plot(shap_values, features=inputs[:50,:], feature_names=names)
 
     save_plot("waterfall_plot.png")
+  
+def makeSomePlots(model, data):
+    """
+    Creates a SHAP summary plot and a beeswarm plot plot for the first prediction.
+    """
+
+    def predict_disc1(data):
+        return model.predict(data)[0][:,0]
+
+    def predict_disc2(data):
+        return model.predict(data)[0][:,1]
+
+    # Need to be conservative about the number of events to make plots
+    # For each event, Shap will remove one variable at a time and rerun inferencing
+    # Modify numEvents below to change the number of points in each plot
+    numEvents = 50
+    inputs = data["inputs"]
+    inputs = inputs[:50,:]
+    print(inputs)
+    names = data["vars"]
+
+    # Use the modified predict_with_model function
+    explainer = shap.KernelExplainer(predict_disc1, inputs, feature_names=names)
+
+    # Selecting 50 events to make the waterfall plot with
+    # Note that we are using 500 perterbations of each event to estimate the average shapely values for that event
+    # Be careful with scaling
+    shap_values = explainer.shap_values(inputs[:50,:], nsamples=500)
+    #explanation = shap.Explanation(
+    #    values=shap_values,
+    #    base_values=explainer.expected_value,
+    #    data=data[instance_index,:],
+    #    feature_names=data["vars"]
+    #)
+    
+    # Changing this to summary plot for now because that seems like the most interesting to me (Bryan)
+    # This should be changed back to waterfall if we want to look at individual events
+    # Generate a waterfall plot for the first prediction
+    shap.waterfall_plot(explainer.expected_value[0], shap_values[0][0], feature_names=names, max_display=10)
+    save_plot("waterfall_plot.png")
+  
+    shap.summary_plot(shap_values, features=inputs[:50,:], feature_names=names)
+    save_plot("summary_plot.png")
+  
+    shap.beeswarm_plot(shap_values, features=inputs[:50,:], feature_names=names)
+    save_plot("beeswarm_plot.png")
   
   
 def waterfall4(explanation):
