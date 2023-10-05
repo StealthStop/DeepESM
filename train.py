a6b4456a9f8de93d2aa93099c28cc3ab0c76ec4a
rileyp336 <131489839+rileyp336@users.noreply.github.com>
2023-10-04 20:55:43 -0500

    giving up on makeSomePlots for now

--- train.py
+++ train.py
@@ -732,7 +732,7 @@ class Train:
         #bgTrainSet = sum( (glob(self.config["dataSet"]+"MyAnalysis_"+bkgd+temp+"Train.root") for bkgd in self.config["bkgd"][1]), [])
         #loader = DataLoader(self.config, sgTrainSet, bgTrainSet)
         data = self.loader.getFlatData()
-        makeSomePlots(model, data)
+        summary_plt(model, data, 0)
         
         metric = val.makePlots(self.doQuickVal, self.config["evalMass"], self.config["evalModel"])
         del val
