1224b49f88587a5644c59a75b840bbb590ac7062
rileyp336 <131489839+rileyp336@users.noreply.github.com>
2023-10-11 19:34:22 -0500

    Adding function that makes multiple shap plots

--- ShapUtils.py
+++ ShapUtils.py
@@ -75,7 +75,7 @@ def summary_plot(model, data, instance_index, outpath):
     save_plot("{}/summary_disc2_plot.png".format(outpath))
 
 
-def bar_and_heat_plots(model, data, outpath):
+def make_shap_plots(model, data, outpath):
     """
     Makes a bar plot and a heat plot.
     """
@@ -94,28 +94,53 @@ def bar_and_heat_plots(model, data, outpath):
     inputs = inputs[:numEvents,:]
     names = data["vars"]
 
-    # Use the modified predict_with_model function
-    explainer = shap.KernelExplainer(predict_disc1, inputs, feature_names=names)
-
-    # Selecting 50 events to make the waterfall plot with
-    # Note that we are using 500 perterbations of each event to estimate the average shapely values for that event
-    # Be careful with scaling
-    shap_values = explainer.shap_values(inputs[:numEvents,:], nsamples=500)
-    
-    # Changing this to summary plot for now because that seems like the most interesting to me (Bryan)
-    # This should be changed back to waterfall if we want to look at individual events
-    shap.plots.bar(shap_values)
-    save_plot("{}/bar_plot_disc1_plot.png".format(outpath))
-    shap.plots.heatmap(shap_values)
-    save_plot("{}/heatmap_disc1_plot.png".format(outpath))
   
+    #Making shap values for disc1
+    explainer = shap.KernelExplainer(predict_disc1, inputs, feature_names=names)
+    shap_values = explainer.shap_values(inputs[:numEvents,:], nsamples=500)
+    explanation = shap.Explanation(values=shap_values, base_values=explainer.expected_value, data=inputs, feature_names=names)
+
+    #making the plots for disc1
+    shap.summary_plot(shap_values, features=inputs[:numEvents,:], feature_names=names)
+    save_plot("summary_plot_disc1_plot.png")
+  
+    shap.summary_plot(shap_values, features=inputs[:numEvents,:], feature_names=names, plot_type="bar")
+    save_plot("bar_plot_disc1_plot.png")
+  
+    shap.plots.heatmap(explanation)
+    save_plot("heatmap_plot_disc1_plot.png")
+  
+    shap.summary_plot(shap_values, features=inputs[:numEvents,:], feature_names=names, plot_type="violin")
+    save_plot("violin_plot_disc1_plot.png")
+  
+    shap.plots.violin(shap_values, features=inputs[:numEvents,:], feature_names=names, plot_type="layered_violin")
+    save_plot("layered_violin_plot_disc1_plot.png")
+
+
+    #Making shap values for disc2
     explainer = shap.KernelExplainer(predict_disc2, inputs, feature_names=names)
     shap_values = explainer.shap_values(inputs[:numEvents,:], nsamples=500)
+    explanation = shap.Explanation(values=shap_values, base_values=explainer.expected_value, data=inputs, feature_names=names)
+
+    #making the plots for disc2
+    shap.summary_plot(shap_values, features=inputs[:numEvents,:], feature_names=names)
+    save_plot("summary_plot_disc2_plot.png")
   
-    shap.plots.bar(shap_values)
-    save_plot("{}/bar_plot_disc2_plot.png".format(outpath))
-    shap.plots.heatmap(shap_values)
-    save_plot("{}/heatmap_disc2_plot.png".format(outpath))
+    shap.summary_plot(shap_values, features=inputs[:numEvents,:], feature_names=names, plot_type="bar")
+    save_plot("bar_plot_disc2_plot.png")
+  
+    shap.plots.heatmap(explanation)
+    save_plot("heatmap_plot_disc2_plot.png")
+  
+    shap.summary_plot(shap_values, features=inputs[:numEvents,:], feature_names=names, plot_type="violin")
+    save_plot("violin_plot_disc2_plot.png")
+  
+    shap.plots.violin(shap_values, features=inputs[:numEvents,:], feature_names=names, plot_type="layered_violin")
+    save_plot("layered_violin_plot_disc2_plot.png")
+
+
+    
+
 
 
 def save_plot(name):
@@ -124,7 +149,3 @@ def save_plot(name):
   """
   plt.savefig(name, bbox_inches='tight', format='png')
   plt.close()
-
-
-
-
