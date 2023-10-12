f3281b19c0242c7c9b28a65fb8ba508f2f9a41d8
rileyp336 <131489839+rileyp336@users.noreply.github.com>
2023-10-11 19:36:23 -0500

    Adding new shap plots

--- train.py
+++ train.py
@@ -27,7 +27,7 @@ from DataLoader import DataLoader
 from Models import main_model
 from MeanShiftTF import MeanShift
 from CustomCallback import CustomCallback
-import ShapUtils
+from ShapUtils import make_shap_plots
 
 
 def timeStamp():
@@ -749,8 +749,7 @@ class Train:
         #bgTrainSet = sum( (glob(self.config["dataSet"]+"MyAnalysis_"+bkgd+temp+"Train.root") for bkgd in self.config["bkgd"][1]), [])
         #loader = DataLoader(self.config, sgTrainSet, bgTrainSet)
         data = self.loader.getFlatData()
-        summary_plot(model, data, 0, self.config["outputDir"])
-        bar_and_heat_plots(model, data, self.config["outputDir"])
+        make_shap_plots(model, data, self.config["outputDir"])
         
         
         metric = val.makePlots(self.doQuickVal, self.config["evalMass"], self.config["evalModel"])
